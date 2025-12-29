7a78a25e2989abb54fbd897d1b39a0425fed9085
karenhirayama <karenm.hirayama@gmail.com>
2025-12-28 22:35:51 -0300

    feat: Update main files

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import SigninPage from "./pages/SigninPage";
+import MainPage from "./pages/MainPage";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+  const [username, setUsername] = useLocalStorage<string | null>(
+    "codeleap_username",
+    null
+  );
 
-export default App
+  const handleSignup = (newUsername: string) => {
+    setUsername(newUsername);
+  };
+
+  const handleLogout = () => {
+    setUsername(null);
+  };
+
+  if (!username) {
+    return <SigninPage onSignup={handleSignup} />;
+  }
+
+  return <MainPage username={username} onLogout={handleLogout} />;
+};
+
+export default App;
